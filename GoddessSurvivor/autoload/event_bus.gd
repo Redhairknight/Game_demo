91ac339643c797c45cab8a03eefe068c435c7d67
Redhairknight <liu523156391@gmail.com>
2026-06-29 15:53:55 +0800

    feat(synergy): add synergy_awakened signal to EventBus

--- GoddessSurvivor/autoload/event_bus.gd
+++ GoddessSurvivor/autoload/event_bus.gd
@@ -65,6 +65,9 @@ signal wave_started(wave_number: int)
 ## Boss出现
 signal boss_spawned(boss_node: Node)
 
+## 协同觉醒触发 (character_id: String, weapon_id: String)
+signal synergy_awakened(character_id: String, weapon_id: String)
+
 
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
